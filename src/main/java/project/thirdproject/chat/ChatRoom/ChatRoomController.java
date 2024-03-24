d548ba6f82ef07be7e6afdf091e47f26c6331a0c
unknown <jjangmi4843@gamil.com>
2024-03-24 17:35:59 +0900

    chattingroom

--- src/main/java/project/thirdproject/chat/ChatRoom/ChatRoomController.java
+++ src/main/java/project/thirdproject/chat/ChatRoom/ChatRoomController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import project.thirdproject.user.SiteUser;
+import project.thirdproject.user.UserCreateForm;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -18,24 +20,27 @@ public class ChatRoomController {
 
     private final ChatRoomService chatRoomService;
     @GetMapping(value = "/rooms")
-    public String rooms(Model model){
+    public String rooms(Principal principal, UserCreateForm userCreateForm, Model model){
+        principal.getName();
         List<ChatRoom> chatRoomList = this.chatRoomService.findAllRooms();
         model.addAttribute("list",chatRoomList);
-        return "chat/rooms";
+//        return "chat/rooms";
+        return "test";
     }
 
 
     //채팅방 개설
     @PostMapping(value = "/room")
-    public String create(@RequestParam String name,Model model){
+    public String create(Principal principal, @RequestParam String name, Model model){
         this.chatRoomService.createRoom(name);
         return "redirect:/chat/rooms";
     }
 
     //채팅방 조회
     @GetMapping("/room")
-    public void getRoom(String roomId, Model model){
+    public String getRoom(Principal principal, String roomId, Model model){
         ChatRoom chatRoom = this.chatRoomService.findByRoomId(Long.valueOf(roomId));
         model.addAttribute("room", chatRoom);
+        return "redirect:/chat/rooms";
     }
 }
